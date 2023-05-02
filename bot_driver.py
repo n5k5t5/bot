3c5c3b4042294354e762fe53e3526a866464377c
n5k5t5 <47543522+n5k5t5@users.noreply.github.com>
2023-05-02 08:53:19 -0400

    Update bot_driver.py

--- bot_driver.py
+++ bot_driver.py
@@ -3,27 +3,16 @@
 A simple demo bot driver
 '''
 import time
-from bot import Pool
-import sys
+from bot import TimelyPool
+import sys, os
 
 
-command = ['./echo_bot.py']
-tasks = list(range(50))
+command = [f'{os.path.dirname(__file__)}/echo_bot.py']
+tasks = list(range(1000000))
 num_workers = 1
+
 setup = 'hello'
-pool = Pool(command, tasks, num_workers, setup=setup, logstream=sys.stdout)
-
-while pool.tasks.unfinished_tasks:
-	print(f'progress: {pool.results.qsize()}')
-	time.sleep(5)
-
-results = [None] * len(tasks)
-while not pool.results.empty():
-	i, value = pool.results.get()
-	results[i] = value
-
-for x, y in zip(tasks, results):
-	print(x, y)
-
-
-
+target_runtime = 100
+tta = time.time() + target_runtime # target time of arrival
+pool = TimelyPool(command, tasks, setup=setup, tta=tta)
+pool.run(num_workers=num_workers, speed_adj=10)
