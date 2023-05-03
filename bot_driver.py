fcd8deb9867b1293623171a7c7b5b213aa8aec6a
n5k5t5 <47543522+n5k5t5@users.noreply.github.com>
2023-05-03 09:25:43 -0400

    Update bot_driver.py

--- bot_driver.py
+++ bot_driver.py
@@ -8,11 +8,16 @@ import sys, os
 
 
 command = [f'{os.path.dirname(__file__)}/echo_bot.py']
-tasks = list(range(1000000))
-num_workers = 1
+tasks = list(range(100000))
 
 setup = 'hello'
 target_runtime = 100
 tta = time.time() + target_runtime # target time of arrival
-pool = TimelyPool(command, tasks, setup=setup, tta=tta)
-pool.run(num_workers=num_workers, speed_adj=10)
+pool = TimelyPool(command, tasks, setup=setup).start(num_workers=4, tta=tta)
+
+while not pool.is_done():
+    time.sleep(5)
+    print(f'progress: {pool.results.qsize()}', flush=True)
+
+results = pool.get_results()
+print(results[:10])
