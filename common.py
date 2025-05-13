505ad3c94e7b6db75b3a6eb038bc2110c259abdd
n5k5t5 <47543522+n5k5t5@users.noreply.github.com>
2025-05-13 15:33:11 -0400

    Update common.py

--- common.py
+++ common.py
@@ -1,65 +1,63 @@
 import os, sys
-import json
 import logging
 from abc import ABC
+from pickle import loads, dumps
+import traceback
+import __main__
 
 
 SOCKET_FILE = '/usr/local/mtxprc/muni_pricing/tmp/socket_{pid}_{suffix}'
 BYTE_ORDER = 'little'
-HEADERSIZE = 8
+INTSIZE = 8
 INIT_DATA = 'INIT_DATA'
 TASK = 'TASK'
-
 log_format = f'pid {os.getpid()} %(filename)s %(asctime)s %(levelname)s: %(message)s'
 datefmt = '%Y%m%d %H:%M:%S'
 
 logging.basicConfig(level=logging.INFO, stream=sys.stdout, format=log_format, datefmt=datefmt)
 
 
-encode_msg = lambda x: json.dumps(x).encode()
-
-
-decode_msg = lambda y: json.loads(y.decode())
+encode_msg = dumps
+decode_msg = loads
 
 
 def send_msg(msg, out_stream):
-    logging.debug(f'Sending msg {msg}')
     raw_msg = encode_msg(msg)
     msg_size = len(raw_msg)
-    header = int.to_bytes(msg_size, HEADERSIZE, BYTE_ORDER)
-    out_stream.write(header)
-    out_stream.write(raw_msg)
+    header = int.to_bytes(msg_size, INTSIZE, BYTE_ORDER)
+    out_stream.write(header + raw_msg)
     out_stream.flush()
-    logging.debug('sent')
 
 
 def read_msg(in_stream):
-    logging.debug(f'Reading msg')
-    header = in_stream.read(HEADERSIZE)
-    logging.debug(f'Read header {header}')
+    header = in_stream.read(INTSIZE)
     if not header:
         logging.info(f'{in_stream} is closed')
         return None, None
-    elif len(header) < HEADERSIZE:
+    elif len(header) < INTSIZE:
         raise Exception(f'Reading: header is too small: size {len(header)}')
     msg_size = int.from_bytes(header, BYTE_ORDER)
-    logging.debug(f'Reading msg body (size {msg_size})')
     raw_msg = in_stream.read(msg_size)
-    logging.debug(f'Read msg body (size {msg_size})')
     msg = decode_msg(raw_msg)
-    logging.debug(f'Read {msg}')
-    return msg, True
+    return msg, INTSIZE + msg_size  # or just anything that has a boolean value of True
+
+
+def parse_msg(msg):
+    header_size = int.from_bytes(msg[:INTSIZE], BYTE_ORDER)
+    return msg[INTSIZE, INTSIZE + header_size], msg[INTSIZE + header_size: 2 * INTSIZE + header_size], msg[2 * INTSIZE + header_size:]
+
+
+def compose_msg(target, idx, arg):
+    return int.to_bytes(len(target), INTSIZE, BYTE_ORDER) + target + int.to_bytes(idx, INTSIZE, BYTE_ORDER) + arg
 
 
 class AbstractWorker(ABC):
-    def connect(self):
-        return
+    def connect(self): ...
 
     def initialize(self, init_data):
         return True
     
-    def do_task(self, task):
-        pass
+    def do_task(self, task): ...
 
     def has_quit(self):
         return False
@@ -79,6 +77,6 @@ class HouseWorker(AbstractWorker):
         ind, ask = task
         try:
             res = self.task_processor(ask)
-        except Exception as error:
-            res = str(error)
+        except Exception:
+            res = traceback.format_exc()
         return ind, res
