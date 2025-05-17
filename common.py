2a7abd33f8b675d9e94b60f6e82e12f7095b8526
n5k5t5 <47543522+n5k5t5@users.noreply.github.com>
2025-05-17 05:54:42 -0400

    Update common.py

--- common.py
+++ common.py
@@ -1,82 +1,102 @@
-import os, sys
-import logging
+import os
 from abc import ABC
-from pickle import loads, dumps
-import traceback
+from pickle import loads as loadb, dumps as dumpb
+from datetime import datetime
 import __main__
 
 
-SOCKET_FILE = '/usr/local/mtxprc/muni_pricing/tmp/socket_{pid}_{suffix}'
+SOCKET_FILE = '/var/run/socket_{pid}_{suffix}'
 BYTE_ORDER = 'little'
 INTSIZE = 8
-INIT_DATA = 'INIT_DATA'
-TASK = 'TASK'
-log_format = f'pid {os.getpid()} %(filename)s %(asctime)s %(levelname)s: %(message)s'
-datefmt = '%Y%m%d %H:%M:%S'
-
-logging.basicConfig(level=logging.INFO, stream=sys.stdout, format=log_format, datefmt=datefmt)
+INIT_DATA = b'initialize'
+TASK = b'do_task'
 
 
-encode_msg = dumps
-decode_msg = loads
+encode_int = lambda x: int.to_bytes(x, INTSIZE, BYTE_ORDER)
+decode_int = lambda y: int.from_bytes(y[:INTSIZE], BYTE_ORDER)
 
 
-def send_msg(msg, out_stream):
-    raw_msg = encode_msg(msg)
-    msg_size = len(raw_msg)
-    header = int.to_bytes(msg_size, INTSIZE, BYTE_ORDER)
-    out_stream.write(header + raw_msg)
+def send_msg(raw_msg: bytes, out_stream):
+    out_stream.write(encode_int(len(raw_msg)) + raw_msg)
     out_stream.flush()
 
 
 def read_msg(in_stream):
     header = in_stream.read(INTSIZE)
     if not header:
-        logging.info(f'{in_stream} is closed')
+        logger.info(f'{in_stream} is closed')
         return None, None
     elif len(header) < INTSIZE:
         raise Exception(f'Reading: header is too small: size {len(header)}')
-    msg_size = int.from_bytes(header, BYTE_ORDER)
+    msg_size = decode_int(header)
     raw_msg = in_stream.read(msg_size)
-    msg = decode_msg(raw_msg)
-    return msg, INTSIZE + msg_size  # or just anything that has a boolean value of True
+    return raw_msg, INTSIZE + msg_size  # or just anything that has a boolean value of True
 
 
-def parse_msg(msg):
-    header_size = int.from_bytes(msg[:INTSIZE], BYTE_ORDER)
-    return msg[INTSIZE, INTSIZE + header_size], msg[INTSIZE + header_size: 2 * INTSIZE + header_size], msg[2 * INTSIZE + header_size:]
+def decompose_msg(msg: bytes):
+    target_len = decode_int(msg)
+    # target, raw_idx, arg
+    return msg[INTSIZE: INTSIZE + target_len], msg[INTSIZE + target_len: 2 * INTSIZE + target_len], msg[2 * INTSIZE + target_len:]
 
 
-def compose_msg(target, idx, arg):
-    return int.to_bytes(len(target), INTSIZE, BYTE_ORDER) + target + int.to_bytes(idx, INTSIZE, BYTE_ORDER) + arg
+def compose_msg(target: bytes, raw_idx: bytes, arg: bytes):
+    return encode_int(len(target)) + target + raw_idx + arg
 
+
+class Logger:
+    try:
+        root_name = os.path.splitext(os.path.basename(__main__.__file__))[0]
+    except:
+        root_name = ''
+    
+    def __init__(self, module_path):
+        self.module_name = os.path.splitext(os.path.basename(module_path))[0]
+        self.error = self._vocal('ERROR')
+        self.warning = self._vocal('WARNING')
+        self.info = self._vocal('INFO')
+        self.debug = self._silent
+
+    def _silent(self, *args, **kwargs): 
+        return
+    
+    def _vocal(self, mode):
+        def f(*args, **kwargs):
+            return print(f'{self.root_name} {self.module_name} {datetime.now().strftime("%Y-%m-%d %H:%M:%S")} {os.getpid()} {mode}:',  *args, **kwargs)
+        return f
+ 
 
 class AbstractWorker(ABC):
     def connect(self): ...
 
-    def initialize(self, init_data):
-        return True
+    def initialize(self, init_data): ...
     
     def do_task(self, task): ...
 
-    def has_quit(self):
-        return False
+    def has_quit(self) -> bool: ...
+
 
 class HouseWorker(AbstractWorker):
-    def __init__(self, task_processor, initializer=None, name='House Worker'):
-        self.task_processor = task_processor
-        self.initializer = initializer
+    def __init__(self, calls, name='House Worker'):
+        '''
+        :param task_processor: a function like object taking one argument and not taking exceptions
+        : param initializer: optional, for backwards
+        '''
+        self.calls = calls
         self.name = name
 
     def initialize(self, init_data):
-        if init_data is not None:
-            self.initializer(*init_data)
-        return True
-    
+        return self.calls[INIT_DATA](init_data)
+        
     def do_task(self, task):
-        ind, ask = task
-        try:
-            res = self.task_processor(ask)
-        except Exception:
-            res = traceback.format_exc()
-        return ind, res
+        return task[0], self.calls[TASK](task[1])
+    
+    def execute(self, msg):
+        target, raw_idx, arg = decompose_msg(msg)
+        func = self.calls[target]
+        return compose_msg(target, raw_idx, dumpb(func(loadb(arg))))
+
+    def has_quit(self):
+        return False
+    
+    
+logger = Logger(__file__)
